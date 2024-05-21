7241a3a589c335bcbba39af724a2a3f062e69cae
shruti082003 <shrutirathod2003@gmail.com>
2024-05-21 13:26:58 +0530

    News-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import Search from './Search';
+import Allnews from './Allnews';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './style.css';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="header">
+            <Router>
+                <Link to='/SearchNews' className="link">SearchNews</Link>
+                <Link to='/Allnews' className="link">AllNews</Link>
+                <Routes>
+                    <Route path='/' element={<Search />} />
+                    <Route path='/SearchNews' element={<Search />} />
+                    <Route path='/Allnews' element={<Allnews />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
